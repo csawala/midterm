072707d712727f369cf356c19f904977aab5a97f
DeeKeilholz <deekeilholz@gmail.com>
2016-11-24 19:20:23 +0000

    post request to login is working plus cookie and encrypt password

--- routes/users.js
+++ routes/users.js
@@ -5,46 +5,49 @@ const router  = express.Router();
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 
+
+
+
 module.exports = (knex) => {
 
- router.get("/", (req, res) => {
-   knex
-     .select("*")
-     .from("users")
-     .then((results) => {
-       res.json(results);
-     });
- });
+router.get("/", (req, res) => {
+  knex
+    .select("*")
+    .from("users")
+    .then((results) => {
+      res.json(results);
+    });
+});
 
- router.get("/signup", (req, res) => {
-   res.render("signup")
- });
+router.get("/signup", (req, res) => {
+  res.render("signup")
+});
 
- router.post("/signup", (req, res) => {
-   if (req.body.email === "" || req.body.password === "") {
-     res.status(400)
-     res.send("Please enter an email and password.");
-  } else {
-    knex('users')
-      .insert({
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password)
-    }, 'id')
-     .then((arrayOfId) => {
-       const id = arrayOfId[0]
-       req.sessions(id)
-       res.redirect("/");
-     })
-     .catch(err => {
-       res.state(400)
-     })
-   }
- });
+router.post("/signup", (req, res) => {
+  if (req.body.email === "" || req.body.password === "") {
+    res.status(400)
+    res.send("Please enter an email and password.")
+ } else {
+   knex('users')
+     .insert({
+       email: req.body.email,
+       password: bcrypt.hashSync(req.body.password, 10),
+   }, 'id')
+    .then((arrayOfId) => {
+      const id = arrayOfId[0]
+      req.session.user_id = id
+      res.redirect("/");
+    })
+    .catch(err => {
+      res.status(400)
+    })
+  }
+});
 
 
- router.get("/login", (req, res) => {
-   res.render("login")
- });
+router.get("/login", (req, res) => {
+  res.render("login")
+});
 
- return router;
+return router;
 }
--- server.js
+++ server.js
@@ -57,6 +57,9 @@ app.get("/", (req, res) => {
 });
 
 
+
+
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
