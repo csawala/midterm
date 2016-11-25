01bcc15eae3c57744f51db4af484f20cd85e5e96
Ghislain H <telitos@gmail.com>
2016-11-25 16:49:27 +0000

    modified the map.js, removed map.css in styles

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -5,38 +5,79 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
-    center: {lat: -34.397, lng: 150.644}
+    center: {lat: 43.680, lng: -79.431}
   });
   var geocoder = new google.maps.Geocoder();
 
-  $('#submit').on('click', function() {
-    geocodeAddress(geocoder, map);
-  });
-}
+  // $('#location').on('click', function() {
+  //   geocodeAddress(geocoder, map);
+  // });
 
-function geocodeAddress(geocoder, resultsMap) {
-  var address = $('#address').val();
+  $('#locate').on('click', function() {
+    getToAddress(geocoder, map);
+  });
+
+  google.maps.event.addListener(map, 'click', function(event) {
+    map.setCenter(event.latLng)
+  })
+
+  google.maps.event.addListener(map, 'click', function(event) {
+   let marker = new google.maps.Marker({
+          map: map,
+          position: event.latLng,
+          title : "Insert title here"
+        });
+    google.maps.event.addListener(marker, "click", function (event) {
+      let latitude = event.latLng.lat();
+      let longitude = event.latLng.lng();
+      console.log( latitude + ', ' + longitude );
+      //$(.marker-form).slidetoggle
+
+      // Center of map
+      map.panTo(new google.maps.LatLng(latitude,longitude));
+
+});
+
+    // alert(event.latLng);
+  })
+
+
+//   google.maps.event.addListener(map, 'click', function(event) {
+//     placeMarker(event.latLng);
+// });
+}
+function getToAddress(geocoder, resultsMap) {
+  var address = $('#location').val();
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === 'OK') {
       resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-      const coordinates = { lat: results[0].geometry.location.lat(),
-        lng: results[0].geometry.location.lng()}
-      $.ajax({
-        url: "/api/maps/map",
-        method: "POST",
-        data: coordinates,
-        success: () => {
-          console.log("Ajax came thru")},
-        error: (err) => {
-          console.log("this is the error:", err)
-        }
-      })
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
     }
-  });
+  })
 }
+
+// function geocodeAddress(geocoder, resultsMap) {
+//   var address = $('#address').val();
+//   geocoder.geocode({'address': address}, function(results, status) {
+//     if (status === 'OK') {
+//       resultsMap.setCenter(results[0].geometry.location);
+//       // var marker = new google.maps.Marker({
+//       //   map: resultsMap,
+//       //   position: results[0].geometry.location
+//       // });
+//       const coordinates = { lat: results[0].geometry.location.lat(),
+//         lng: results[0].geometry.location.lng()}
+//       $.ajax({
+//         url: "/api/maps/map",
+//         method: "POST",
+//         data: coordinates,
+//         success: () => {
+//           console.log("Ajax came thru")},
+//         error: (err) => {
+//           console.log("this is the error:", err)
+//         }
+//       })
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//     }
+//   });
+// }
