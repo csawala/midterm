46c0bb4e7009355b6c1afd801ee06577a61fc388
Ghislain H <telitos@gmail.com>
2016-11-25 17:37:22 +0000

    map.js changes

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -3,11 +3,11 @@
 
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
     center: {lat: 43.680, lng: -79.431}
   });
-  var geocoder = new google.maps.Geocoder();
+  const geocoder = new google.maps.Geocoder();
 
   // $('#location').on('click', function() {
   //   geocodeAddress(geocoder, map);
@@ -28,8 +28,33 @@ function initMap() {
           title : "Insert title here"
         });
     google.maps.event.addListener(marker, "click", function (event) {
+      let infowindow = new google.maps.InfoWindow;
       let latitude = event.latLng.lat();
       let longitude = event.latLng.lng();
+      geocodeLatLng(geocoder, map, infowindow)
+
+        function geocodeLatLng(geocoder, map, infowindow) {
+        // var input = document.getElementById('latlng').value;
+        // var latlngStr = input.split(',', 2);
+        var latlng = {lat: latitude, lng: longitude};
+        geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+            if (results[1]) {
+              // map.setZoom(11);
+              // var marker = new google.maps.Marker({
+              //   position: latlng,
+              //   map: map
+              // });
+              infowindow.setContent(results[1].formatted_address);
+              infowindow.open(map, marker);
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
+      }
       console.log( latitude + ', ' + longitude );
       //$(.marker-form).slidetoggle
 
