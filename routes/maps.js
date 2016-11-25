1f43c541c91c0040090cb543e4c7b01c2b7a326a
csawala <chris.sawala@gmail.com>
2016-11-25 23:44:25 +0000

    updated maps.js to push and pull proper coordinates to db

--- routes/maps.js
+++ routes/maps.js
@@ -2,8 +2,13 @@
 
 const express = require('express');
 const router  = express.Router();
+const bodyParser  = require("body-parser");
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 module.exports = (knex) => {
+  const st = require('knex-postgis')(knex)
 
   router.get("/", (req, res) => {
     const templateVars = { MAP_API: process.env.MAP_API }
@@ -11,11 +16,32 @@ module.exports = (knex) => {
   });
 
   router.post("/map", (req, res) => {
-      // knex("positions")
-      //   .insert({
-      //     posiiton: body
-      //   })
+    console.log("body: ", req.body)
+    let coordX = Number(req.body.pointX)
+    let coordY = Number(req.body.pointY)
+    console.log(coordX, coordY)
+    // let userId = req.body.
+    // let mapId = req.body.
+    // let title = req.body.
+    // let desc = req.body.
+    // let img = req.body.
+
+
+    knex.insert({
+      mapid: 2,
+      loc: st.geomFromText(`Point(${coordX} ${coordY})`, 4326),
+      title: 'testing title',
+      info: 'testing description',
+      createdby: 2,
+      image: 'http://www.img.com'
+    }).into('points')
+    .then(() => {
+      knex.select('*', st.asText('loc')).from('points')
+      .then((event) => {
+        console.log(event)
       })
+    })
+  })
 
   router.get("/mapbox", (req, res) => {
     const templateVars = {MAP_BOX_API_PRIVATE: process.env.MAP_BOX_API_PRIVATE,
