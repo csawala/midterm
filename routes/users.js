b1423daee454bc1c7db00ac991387f4852cfc8c3
DeeKeilholz <deekeilholz@gmail.com>
2016-11-25 19:54:02 +0000

    added changes to index

--- routes/users.js
+++ routes/users.js
@@ -20,14 +20,15 @@ router.get("/", (req, res) => {
     });
 });
 
+// Requests for sign up
+
 router.get("/signup", (req, res) => {
   res.render("signup")
 });
 
 router.post("/signup", (req, res) => {
   if (req.body.email === "" || req.body.password === "") {
-    res.status(400)
-    res.send("Please enter an email and password.")
+    res.render("signup", {errorMsg: "Please enter a username and password."});
  } else {
    knex('users')
      .insert({
@@ -45,6 +46,7 @@ router.post("/signup", (req, res) => {
   }
 });
 
+// Requests for login
 
 router.get("/login", (req, res) => {
   res.render("login")
@@ -57,27 +59,32 @@ router.post("/login", (req, res) => {
   .select('email', 'password')
   .where('email', req.body.email)
   .then((info) => {
+    console.log("info: ", info)
     if (info.length === 0) {
-    res.status(400)
-    res.send("Please enter an email.");
-    res.redirect("/");
+      res.render("login", {errorMsg: "Please enter a username and password."});
     } else if (bcrypt.compareSync(req.body.password, info[0].password)) {
-    knex('users')
-    .select('id')
-    .where('email', req.body.email)
-    .then( (arrayOfId) => {
-      const userId = arrayOfId[0]
-      console.log(arrayOfId[0]);
-      req.session.id = userId
-      res.redirect("/");
-      })
+      knex('users')
+      .select('id')
+      .where('email', req.body.email)
+      .then( (arrayOfId) => {
+        const userId = arrayOfId[0]
+        console.log("arrayofId: ",arrayOfId);
+        req.session.id = userId
+        res.redirect("/");
+        })
     } else {
-      res.status(400)
-      res.send("Did you forget your password?")
-      res.redirect("/");
+      res.render("login", {errorMsg: "Did you forget your password?"});
     }
   });
 });
+
+// Request for logout
+
+router.post("/", (req, res) => {
+  req.session = null;
+  res.redirect("/api/users/login");
+});
+
 
 return router;
 }
