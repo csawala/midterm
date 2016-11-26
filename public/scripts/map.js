5bf6f4cae8b0d622027df1a112b108d31127f8fe
Ghislain H <telitos@gmail.com>
2016-11-26 01:49:22 +0000

    small map.js modification

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -138,38 +138,38 @@ function initMap() {
 //     placeMarker(event.latLng);
 // });
 }
-function getToAddress(geocoder, resultsMap) {
-  var address = $('#location').val();
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
-    }
-  })
-}
+// function getToAddress(geocoder, resultsMap) {
+//   var address = $('#location').val();
+//   geocoder.geocode({'address': address}, function(results, status) {
+//     if (status === 'OK') {
+//       resultsMap.setCenter(results[0].geometry.location);
+//     }
+//   })
+// }
 
-function geocodeAddress(geocoder, resultsMap) {
-  var address = $('#address').val();
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
-      // var marker = new google.maps.Marker({
-      //   map: resultsMap,
-      //   position: results[0].geometry.location
-      // });
-      const coordinates = { lat: results[0].geometry.location.lat(),
-        lng: results[0].geometry.location.lng()}
-      $.ajax({
-        url: "/api/maps/map",
-        method: "POST",
-        data: coordinates,
-        success: () => {
-          console.log("Ajax came thru")},
-        error: (err) => {
-          console.log("this is the error:", err)
-        }
-      })
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
+// function geocodeAddress(geocoder, resultsMap) {
+//   var address = $('#address').val();
+//   geocoder.geocode({'address': address}, function(results, status) {
+//     if (status === 'OK') {
+//       resultsMap.setCenter(results[0].geometry.location);
+//       // var marker = new google.maps.Marker({
+//       //   map: resultsMap,
+//       //   position: results[0].geometry.location
+//       // });
+//       const coordinates = { lat: results[0].geometry.location.lat(),
+//         lng: results[0].geometry.location.lng()}
+//       $.ajax({
+//         url: "/api/maps/map",
+//         method: "POST",
+//         data: coordinates,
+//         success: () => {
+//           console.log("Ajax came thru")},
+//         error: (err) => {
+//           console.log("this is the error:", err)
+//         }
+//       })
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//     }
+//   });
+// }
